66c37e52d8ca14b1abf58ab51ee81e4db6ace914
darrenalexander <darrenm2@illinois.edu>
2019-07-26 12:46:37 -0700

    Updates

---
+++ precache-manifest.9b850d6ee4d06cda1b6a9b9059e10e71.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa1456df8f95133f41948aae96237f1",
+    "url": "/LostAndFound/index.html"
+  },
+  {
+    "revision": "32cf0c9842e2db95d04d",
+    "url": "/LostAndFound/static/css/2.e828229e.chunk.css"
+  },
+  {
+    "revision": "2581995b7b06fbf5a80a",
+    "url": "/LostAndFound/static/css/main.386ad2a7.chunk.css"
+  },
+  {
+    "revision": "32cf0c9842e2db95d04d",
+    "url": "/LostAndFound/static/js/2.92c0886a.chunk.js"
+  },
+  {
+    "revision": "2581995b7b06fbf5a80a",
+    "url": "/LostAndFound/static/js/main.b95d82e5.chunk.js"
+  },
+  {
+    "revision": "b8601d6fd8e43b1b0b91",
+    "url": "/LostAndFound/static/js/runtime~main.3ac0bbb9.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/LostAndFound/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/LostAndFound/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/LostAndFound/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/LostAndFound/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/LostAndFound/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/LostAndFound/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "9db907da0ec2e86581b029e33eb25c6a",
+    "url": "/LostAndFound/static/media/googleButton.9db907da.png"
+  },
+  {
+    "revision": "95f0c75e0f9fd2bda793e03395582016",
+    "url": "/LostAndFound/static/media/homepage.95f0c75e.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/LostAndFound/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/LostAndFound/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/LostAndFound/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/LostAndFound/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/LostAndFound/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/LostAndFound/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/LostAndFound/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/LostAndFound/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/LostAndFound/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/LostAndFound/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
